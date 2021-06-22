e40c34c499cf82aa93ddfbe0bc3d129f39776a5c
Rafael Borges <rafaelborges.engcomp@gmail.com>
2021-06-21 22:09:48 -0300

    aula2

--- node/src/server.ts
+++ node/src/server.ts
@@ -1,17 +1,18 @@
 import express from "express";
+import "reflect-metadata";
+
+import { router } from "./service/routes"
 
 //@types/express
 const app=express();
 
-app.get("/test", (req,resp)=>{
-    return resp.send("ola");
-})
+app.use(express.json());
 
-app.post("/test", (req,resp)=>{
-    return resp.send("ola post");
-})
+import "./database"
+
+app.use(router);
 
 //começa a executar a aplicação na porta 3000
 app.listen(3000, ()=> {
-    console.log("server escutando porta 3000");
+    console.log("\n*****===server escutando porta 3000===*****\n");
 })
